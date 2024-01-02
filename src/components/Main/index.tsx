efe345694b3fc7a02f0eb5dd7df9fd56e58ab8e7
Dacio Lima <contato.dacio@gmail.com>
2024-01-02 12:14:46 -0300

    implmentação storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,11 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({ title = 'React Avançado', description = 'Reativo' }) => (
   <S.Wrapper>
-    <h1>React</h1>
+    <S.Logo src="/img/logoRc.svg" alt="Imagem logo Next" />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="/img/logo.svg" alt="Um desenvolvedor"></S.Illustration>
   </S.Wrapper>
 )
 
